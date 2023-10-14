6aecca2f917dd9e2dcac9c2e23eb9051713cdbc4
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-14 15:32:14 +0500

    [Start learning about Big-O notation]

---
+++ algorithms.js
@@ -0,0 +1,13 @@
+/*
+
+    Asymptotic notation:
+
+        It is a mathematical tool to represent time and space complexity.   
+        There are three asymptotic notation.
+
+        1) Big-O Notation (O-notation) - Worst case complexity
+        2) Omega Notation - Best case complexity
+        3) Theta Notation - Average case complexity
+
+
+*/
