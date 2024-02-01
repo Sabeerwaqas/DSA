34416bb7f5559462357a450ef6e3c4ee4d14e6d2
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-02-01 22:40:29 +0500

    [Problem solving: Write a program to calculate percentage]

--- Problems/Addregate marks.cpp
+++ Problems/Addregate marks.cpp
@@ -12,89 +12,113 @@ using namespace std;
 	a student is 100.
 	*/
 	
-	// Function declaration for getting marks
-	
-		// Subject 1
-	
-		void subOneFunc(string subOne, int subOneMarks){
-			
-		cout << "Subject=> " << subOne << " " << "Marks=> " << subOneMarks << endl; 
-		
-			}
-	
-		// Subject 2
-	
-		void subTwoFunc(string subTwo, int subTwoMarks){
-			
-		cout << "Subject=> " << subTwo << " " << "Marks=> " << subTwoMarks << endl; 
-			
-			}
-	
-		// Subject 3
-	
-		void subThreeFunc(string subThree, int subThreeMarks){
-			
-		cout << "Subject=> " << subThree << " " << "Marks=> " << subThreeMarks << endl; 
-			
-		}
-	
-		// Subject 4
-	
-		void subFourFunc(string subFour, int subFourMarks){
-			
-		cout << "Subject=> " << subFour << " " << "Marks=> " << subFourMarks << endl; 
-			
-		}
-	
-		// Subject 5
-	
-		void subFiveFunc(string subFive, int subFiveMarks){
-			
-		cout << "Subject=> " << subFive << " " << "Marks=> " << subFiveMarks << endl; 
-			
-		}
-
-
-
 
 int main(){
 	
-	string subOne = "Data Communication";
-	int subOneMarks = 75;
+	// Subject 1
 	
-	string subTwo = "Statistics";
-	int subTwoMarks = 70;
+	string subOne;
+	int subOneMarks;
 	
-	string subThree = "Algorithms";
-	int subThreeMarks = 80;
+	// Subject 2
 	
-	string subFour = "DSA";
-	int subFourMarks = 70;
+	string subTwo;
+	int subTwoMarks;
 	
-	string subFive = "Programming";
-	int subFiveMarks = 70;
+	// Subject 3
 	
-	int totalMarks;
+	string subThree;
+	int subThreeMarks;
+	
+	// Subject 4
+	
+	string subFour;
+	int subFourMarks;
+	
+	// Subject 5
+	
+	string subFive;
+	int subFiveMarks;
+	
+	int totalMarks = 500;
+	int marksObtained;
 	int percentage;
 	
-	cout << "Enter the name and marks of the first subject: " << endl;
-	subOneFunc(subOne, subOneMarks);
-	cout << " " << endl;
-	cout << "Enter the name and marks of the second subject: " << endl;
-	subTwoFunc(subTwo, subTwoMarks);
-	cout << " " << endl;
-	cout << "Enter the name and marks of the third subject: " << endl;
-	subThreeFunc(subThree, subThreeMarks);
-	cout << " " << endl;
-	cout << "Enter the name and marks of the fourth subject: " << endl;
-	subFourFunc(subFour, subFourMarks);
-	cout << " " << endl;
-	cout << "Enter the name and marks of the fiftht subject: " << endl;
-	subFiveFunc(subFive, subFiveMarks);
+	// Getting Subject 1 details:
 	
-	totalMarks = subOne + subTwo + subThree + subFour + subFive;
-	cout << "Total marks: " << totalMarks << endl;
-	cout << "Percentage: " << toatalMarks * 100 / 500 << endl;
+	cout << "Enter the name and marks of the first subject: " << endl;
+	cout << "Subject's name: ";
+	cin >> subOne;
+	
+	cout << " " << endl;
+
+	cout << "Subject's marks: ";
+	cin >> subOneMarks;
+	
+	cout << " " << endl;
+	
+	// Getting Subject 2 details:
+	
+	cout << "Enter the name and marks of the second subject: " << endl;
+	cout << "Subject's name: ";
+	cin >> subTwo;
+	
+	cout << " " << endl;
+
+	cout << "Subject's marks: ";
+	cin >> subTwoMarks;
+	
+	cout << " " << endl;
+	
+	// Getting Subject 3 details:
+	
+	cout << "Enter the name and marks of the third subject: " << endl;
+	cout << "Subject's name: ";
+	cin >> subThree;
+	
+	cout << " " << endl;
+
+	cout << "Subject's marks: ";
+	cin >> subThreeMarks;
+	
+	cout << " " << endl;
+	
+	// Getting Subject 4 details:
+	
+	cout << "Enter the name and marks of the fourth subject: " << endl;
+	cout << "Subject's name: ";
+	cin >> subFour;
+	
+	cout << " " << endl;
+
+	cout << "Subject's marks: ";
+	cin >> subFourMarks;
+	
+	cout << " " << endl;
+	
+	// Getting Subject 5 details:
+	
+	cout << "Enter the name and marks of the fifth subject: " << endl;
+	cout << "Subject's name: ";
+	cin >> subFive;
+	
+	cout << " " << endl;
+
+	cout << "Subject's marks: ";
+	cin >> subFiveMarks;
+	
+	cout << " " << endl;
+		
+	marksObtained = subOneMarks + subTwoMarks + subThreeMarks + subFourMarks + subFiveMarks;
+	
+	cout << "Marks obtained: " << marksObtained << endl;
+	
+	// Percentage
+	
+	percentage = marksObtained * 100 / totalMarks;
+	
+	cout << "Total secured percentage is: " << percentage << " %" << endl;
+	
 	
 	return 0;
 }
